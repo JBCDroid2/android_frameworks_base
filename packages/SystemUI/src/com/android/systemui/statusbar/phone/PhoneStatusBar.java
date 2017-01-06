7423f5c48cc69778ea4a9d552a24109e3e5a09f0
Thecrazyskull <anaskarbila@gmail.com>
2017-01-06 05:09:59 -0600

    SystemUI: update volume dialog when re-inflating views
    Change-Id: I8c7622cb7043823ada78a1088d027f4ce9fa9cf7
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1207,6 +1207,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     initSignalCluster(mHeader);
                     mHeader.setActivityStarter(PhoneStatusBar.this);
                     mStackScroller.updateNotificationView();
+                    mVolumeComponent.updateDialog();
                 }
             });
         }

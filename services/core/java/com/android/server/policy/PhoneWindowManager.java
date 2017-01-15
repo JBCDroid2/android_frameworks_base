c20f279d7353bc171869b4238bb275c078e33f1f
BirdmanJ <gustavocamposdesouzaalves@gmail.com>
2017-01-15 13:22:52 +0000

    Revert "Alert Slider for Oneplus devices"
    This reverts commit c258300a181b686089fa89371644ad291851c426.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -81,7 +81,6 @@ import android.media.IAudioService;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.media.session.MediaSessionLegacyHelper;
-import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
@@ -108,7 +107,6 @@ import android.provider.Settings;
 import android.service.dreams.DreamManagerInternal;
 import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
-import android.service.notification.ZenModeConfig;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
 import android.util.DisplayMetrics;
@@ -387,7 +385,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     AccessibilityManager mAccessibilityManager;
     BurnInProtectionHelper mBurnInProtectionHelper;
     AppOpsManager mAppOpsManager;
-    AlertSliderObserver mAlertSliderObserver;
     private boolean mHasFeatureWatch;
 
     // Vibrator pattern for haptic feedback of a long press.
@@ -952,9 +949,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     };
 
     class SettingsObserver extends ContentObserver {
-        private final Uri SWAP_ALERT_SLIDER_ORDER_URI =
-                Settings.System.getUriFor(Settings.System.ALERT_SLIDER_ORDER);
-
         SettingsObserver(Handler handler) {
             super(handler);
         }
@@ -1067,21 +1061,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(CMSettings.System.getUriFor(
                     CMSettings.System.NAVBAR_LEFT_IN_LANDSCAPE), false, this,
                     UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.ALERT_SLIDER_ORDER), false, this,
-                    UserHandle.USER_ALL);
             updateSettings();
         }
 
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            if (SWAP_ALERT_SLIDER_ORDER_URI.equals(uri)
-                    && mSystemReady && mAlertSliderObserver != null) {
-                mAlertSliderObserver.update();
-            } else {
-                updateSettings();
-                updateRotation(false);
-            }
+        @Override public void onChange(boolean selfChange) {
+            updateSettings();
+            updateRotation(false);
         }
     }
 
@@ -7781,11 +7766,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         // Ensure observe happens in systemReady() since we need
         // CMHardwareService to be up and running
         mSettingsObserver.observe();
-
-        if (ZenModeConfig.hasAlertSlider(mContext)) {
-            mAlertSliderObserver = new AlertSliderObserver(mContext);
-            mAlertSliderObserver.startObserving(com.android.internal.R.string.alert_slider_uevent_match_path);
-        }
 
         readCameraLensCoverState();
         updateUiMode();

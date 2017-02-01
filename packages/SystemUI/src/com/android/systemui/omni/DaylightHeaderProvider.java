fb8c212d6662fb1bb8d9ae09724974426cb8dd48
BirdmanJ <gustavocamposdesouzaalves@gmail.com>
2017-02-01 16:26:30 +0000

    Fix

--- packages/SystemUI/src/com/android/systemui/omni/DaylightHeaderProvider.java
+++ packages/SystemUI/src/com/android/systemui/omni/DaylightHeaderProvider.java
@@ -50,7 +50,7 @@ import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.android.systemui.R;
-import com.android.internal.util.aicp.PackageUtils;
+import com.android.internal.util.viper.PackageUtils;
 
 public class DaylightHeaderProvider implements
         StatusBarHeaderMachine.IStatusBarHeaderProvider {
